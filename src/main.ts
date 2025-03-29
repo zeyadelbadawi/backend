df946386fe566adeefac495ca05df4abd7f82882
ZiadElbadawi <zeyadelbadawi.ze@gmail.com>
2025-03-29 10:09:51 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions: CorsOptions = {
-    origin: ['https://front-end-five-vert.vercel.app/'], 
+    origin: process.env.CORS_ORIGIN ,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
     allowedHeaders: ['Content-Type', 'Authorization'],  
     credentials: true, 
